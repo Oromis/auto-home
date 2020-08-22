1b3dacb7f449b272c4c1356930c8f5aae2c456df
David <david.bauske@googlemail.com>
2020-08-22 13:55:17 +0200

    Initial commit

---
+++ main/main.cpp
@@ -0,0 +1,41 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <cstdio>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "Arduino.h"
+#include "FastLED.h"
+
+CRGB leds[30];
+
+void initLeds() {
+  FastLED.addLeds<NEOPIXEL, 12>(leds, 30);
+  FastLED.setTemperature(Tungsten100W);
+}
+
+extern "C" [[noreturn]] void app_main()
+{
+  initArduino();
+
+  initLeds();
+  Serial.begin(115200, SERIAL_8N1);
+
+  Serial.printf("Hello from Arduino!\n");
+  int cycle_count = 0;
+  uint8_t hue = 0;
+
+  while (true) {
+    delay(33);
+    ++cycle_count;
+
+    FastLED.showColor(CHSV(++hue, 255, 255), 64);
+  }
+}
